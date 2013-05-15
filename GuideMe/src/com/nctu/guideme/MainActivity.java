4aaf68695977b7ab438fadc97e797e8529954a1a
Oscar Juarez <ossskkar@gmail.com>
2013-05-16 01:13:16 +0800

    create BaseActivity

--- GuideMe/src/com/nctu/guideme/MainActivity.java
+++ GuideMe/src/com/nctu/guideme/MainActivity.java
@@ -9,8 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
-		setContentView(R.layout.record_a_path);
+		setContentView(R.layout.initial_screen);
 	}
 
 	@Override
