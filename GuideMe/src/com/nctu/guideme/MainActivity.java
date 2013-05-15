b821db75a4ffddbaef4555b860fe3484d58019f7
Oscar Juarez <ossskkar@gmail.com>
2013-05-16 02:26:47 +0800

    start connecting activities with layouts

--- GuideMe/src/com/nctu/guideme/MainActivity.java
+++ GuideMe/src/com/nctu/guideme/MainActivity.java
@@ -1,15 +1,43 @@
 package com.nctu.guideme;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
 
+	Button recordAPath_button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.initial_screen);
+		
+		/* Create buttons */
+		//Button recordAPath_button   = (Button)findViewById(R.id.recordAPath_button);
+		recordAPath_button   = (Button)findViewById(R.id.recordAPath_button);
+		Button getDirections_button = (Button)findViewById(R.id.getDirections_button);
+		Button settings_button      = (Button)findViewById(R.id.settings_button);
+		Button exit_button          = (Button)findViewById(R.id.exit_button);
+		Button panic_button         = (Button)findViewById(R.id.panicButton_button);
+		
+		
+		/* Implement listeners */
+		recordAPath_button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startActivity(new Intent(getApplicationContext(), RecordAPath_layoutActivity.class));
+				recordAPath_button.setText("Clicked");
+			}
+		});
+		
+		exit_button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 
 	@Override
--- GuideMe/src/com/nctu/guideme/RecordAPath_layoutActivity.java
+++ GuideMe/src/com/nctu/guideme/RecordAPath_layoutActivity.java
@@ -1,7 +1,14 @@
 package com.nctu.guideme;
 
+import com.nctu.guideme.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class RecordAPath_layoutActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.record_a_path);
+	}
 }
