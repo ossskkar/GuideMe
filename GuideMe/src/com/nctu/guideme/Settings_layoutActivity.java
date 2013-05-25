278060df38121971c6d00f0a060c88c9455b963f
Oscar Juarez <ossskkar@gmail.com>
2013-05-26 01:42:32 +0800

    change theme of layouts

--- GuideMe/src/com/nctu/guideme/Settings_layoutActivity.java
+++ GuideMe/src/com/nctu/guideme/Settings_layoutActivity.java
@@ -25,7 +25,7 @@ public class Settings_layoutActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.settings);
+		setContentView(R.layout.emergency_contact);
 		
 		/* Find id of views */
 		contactName_editText = (EditText) findViewById(R.id.contactName_editText);
